3addaa296ac352f8eda28ce26af8d774c567c233
Orcha <or.chaimi4@gmail.com>
2026-03-04 10:37:10 +0200

    update index, css and js. New favicon

---
+++ js/faviconAnimator.js
@@ -0,0 +1,54 @@
+(function ()
+{
+    var favicon = document.getElementById("favicon");
+    var onHref = "assets/favicon-on.png";
+    var offHref = "assets/favicon-off.png";
+
+    var intervalId = null;
+    var showOn = false;
+
+    function setFavicon(href)
+    {
+        favicon.href = href + "?v=" + Date.now();
+    }
+
+    function stop()
+    {
+        if (intervalId !== null)
+        {
+            clearInterval(intervalId);
+            intervalId = null;
+        }
+    }
+
+    function startBlink()
+    {
+        stop();
+
+        intervalId = setInterval(function ()
+        {
+            showOn = !showOn;
+            setFavicon(showOn ? onHref : offHref);
+        }, 700);
+    }
+
+    function setOn()
+    {
+        stop();
+        setFavicon(onHref);
+    }
+
+    document.addEventListener("visibilitychange", function ()
+    {
+        if (document.hidden)
+        {
+            startBlink();
+            return;
+        }
+
+        setOn();
+    });
+
+    window.addEventListener("blur", startBlink);
+    window.addEventListener("focus", setOn);
+})();
